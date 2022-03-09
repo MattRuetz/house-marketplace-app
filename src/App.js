a14911b4ea03cbb3c6a93dc1d1a1e1588ba780c9
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-09 18:56:21 -0500

    private route to /profile, redirects to sign-in if not signed in. Connected firebase Auth status to hook states.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
@@ -9,6 +10,7 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import NotFound from './pages/NotFound';
+
 function App() {
     return (
         <>
@@ -16,7 +18,11 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Explore />}></Route>
                     <Route path="/offers" element={<Offers />}></Route>
-                    <Route path="/profile" element={<Profile />}></Route>
+                    <Route path="/profile" element={<PrivateRoute />}>
+                        {/* This is <Outlet/> component of private route */}
+                        {/* Only show if user is logged in, else go to /sign-in */}
+                        <Route path="/profile" element={<Profile />}></Route>
+                    </Route>
                     <Route path="/sign-in" element={<SignIn />}></Route>
                     <Route path="/sign-up" element={<SignUp />}></Route>
                     <Route
