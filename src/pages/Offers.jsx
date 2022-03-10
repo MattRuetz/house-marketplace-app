9c96e32982e0677af4c227f2d0db4f1d0774dd62
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-10 11:37:03 -0500

    offers page, wueries DB for listings with a discount offer and displayes

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,5 +1,89 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+    collection,
+    getDocs,
+    query,
+    where,
+    orderBy,
+    limit,
+    startAfter,
+} from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import Spinner from '../components/Spinner';
+import ListingItem from '../components/ListingItem';
+
 function Offers() {
-    return <div>Offers</div>;
+    const [listings, setListings] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const params = useParams();
+
+    useEffect(() => {
+        const fetchListings = async () => {
+            try {
+                // Get Listings collection from DB
+                const listingsRef = collection(db, 'listings');
+                //Get 10 listings of with a SALE OFFER
+                const q = query(
+                    listingsRef,
+                    where('offer', '==', true),
+                    orderBy('timestamp', 'desc'),
+                    limit(10)
+                );
+
+                // Execute Query
+                const querySnap = await getDocs(q);
+
+                let listings = [];
+
+                // destucture listings from DB into the listings array
+                querySnap.forEach((doc) => {
+                    return listings.push({
+                        id: doc.id,
+                        data: doc.data(),
+                    });
+                });
+
+                setListings(listings);
+                setLoading(false);
+            } catch (error) {
+                toast.error('Could not fetch listings');
+            }
+        };
+
+        fetchListings();
+    }, [params.categoryName]); //End of useEffect
+
+    return (
+        <div className="category">
+            <header>
+                <p className="pageHeader">Offers</p>
+            </header>
+
+            {loading ? (
+                <Spinner />
+            ) : listings && listings.length > 0 ? (
+                <>
+                    <main>
+                        <ul className="categoryListings">
+                            {/* Listing Components -- TODO */}
+                            {listings.map((listing) => (
+                                <ListingItem
+                                    listing={listing.data}
+                                    id={listing.id}
+                                    key={listing.id}
+                                />
+                            ))}
+                        </ul>
+                    </main>
+                </>
+            ) : (
+                <p>There are no current offers.</p>
+            )}
+        </div>
+    );
 }
 
 export default Offers;
